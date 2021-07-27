7265be467ba3f8f61e578fef935cdc640eb92f39
JinAh1210 <ovewknds@naver.com>
2021-07-27 12:50:04 +0900

    gulp-bro, uglifyify
    gulp-browerify가 더 이상 업데이트를 지원하지 않기 때문에 gulp-browerify대신 gulp-bro를 사용.
    추가로 uglifyify 설치

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,7 +4,8 @@ import nodeSass from "node-sass";
 import autoprefixer from "gulp-autoprefixer";
 import minifyCSS from "gulp-csso";
 import del from "del";
-import bro from "gulp-browserify";
+import bro from "gulp-bro";
+import babel from "babelify";
 
 const sass = gulpSass(nodeSass);
 
@@ -36,7 +37,17 @@ const styles = () =>
     .pipe(gulp.dest(paths.styles.dest));
 
 const js = () =>
-  gulp.src(paths.js.src).pipe(bro()).pipe(gulp.dest(paths.js.dest));
+  gulp
+    .src(paths.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babel.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(paths.js.dest));
 
 const watchFiles = () => {
   gulp.watch(paths.styles.watch, styles);
