8f27de6f1765e9aaff7c5126a28ecdf522ad6808
JinAh1210 <ovewknds@naver.com>
2021-07-27 11:52:28 +0900

    socketIO, gulp, browserify

---
+++ gulpfile.babel.js
@@ -0,0 +1,48 @@
+import gulp from "gulp";
+import gulpSass from "gulp-sass";
+import nodeSass from "node-sass";
+import autoprefixer from "gulp-autoprefixer";
+import minifyCSS from "gulp-csso";
+import del from "del";
+import bro from "gulp-browserify";
+
+const sass = gulpSass(nodeSass);
+
+const paths = {
+  styles: {
+    src: "assets/scss/styles.scss",
+    dest: "src/static/styles",
+    watch: "assets/scss/**/*.scss",
+  },
+  js: {
+    src: "assets/js/main.js",
+    dest: "src/static/js",
+    watch: "assets/js/**/*.js",
+  },
+};
+
+const clean = () => del("src/static");
+
+const styles = () =>
+  gulp
+    .src(paths.styles.src)
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(minifyCSS())
+    .pipe(gulp.dest(paths.styles.dest));
+
+const js = () =>
+  gulp.src(paths.js.src).pipe(bro()).pipe(gulp.dest(paths.js.dest));
+
+const watchFiles = () => {
+  gulp.watch(paths.styles.watch, styles);
+  gulp.watch(paths.js.watch, js);
+};
+
+const dev = gulp.series([clean, styles, js, watchFiles]);
+
+export default dev;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,34 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
+let sockets = [];
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 app.get("/", (req, res) => res.render("home"));
 
-const handleListenig = () => {
+const handleListening = () => {
   console.log(`✔ Server running : http://localhost:${PORT}`);
 };
 
-app.listen(PORT, handleListenig);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anonymous",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
