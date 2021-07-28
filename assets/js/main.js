51f7bef191491d1c9196ad142649426872698d62
JinAh1210 <ovewknds@naver.com>
2021-07-28 17:40:45 +0900

    js : login
    user가 닉네임을 가지고 있으면 loggedIn 화면을 보여주고, 닉네임이 없다면 logIn을 할 수 있는 화면을 보여줌.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,28 @@
-// eslint-disable-next-line no-undef
-import { handleMessageNotif } from "./chat";
-const socket = io("/");
+const body = document.querySelector("body");
+const loginForm = document.getElementById("jsLogin");
 
-function sendMessage(message) {
-  socket.emit("newMessage", { message });
-  console.log(`You: ${message}`);
-}
-function setNickname(nickname) {
-  socket.emit("setNickname", { nickname });
+//key값에 문자열을 직접 입력하지 않고 변수화.
+const NICKNAME = "nickname";
+const LOGGED_OUT = "loggedOut";
+const LOGGED_IN = "loggedIn";
+
+const nickname = localStorage.getItem(NICKNAME);
+
+if (nickname == null) {
+  body.className = LOGGED_OUT;
+} else {
+  body.className = LOGGED_IN;
 }
 
-socket.on("messageNotif", handleMessageNotif);
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const input = loginForm.querySelector("input");
+  const { value } = input; // input.value
+  input.value = "";
+  //username저장
+  localStorage.setItem(NICKNAME, value);
+};
+
+if (loginForm) {
+  loginForm.addEventListener("submit", handleFormSubmit);
+}
